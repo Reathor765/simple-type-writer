fffe560862e10aea5cf07aa91282161ee4330f50
Louis Vidal <65010978+Pignomaster@users.noreply.github.com>
2025-10-05 16:58:43 +0200

    Seperate editor/runtime code for type writer label class

--- addons/type_writer_label/classes/type_writer_label.gd
+++ addons/type_writer_label/classes/type_writer_label.gd
@@ -24,6 +24,7 @@ var _typing_timer: float = 0.0
 var _pause_timer: float = 0.0
 var _stopped: bool = false
 
+
 # INSPECTOR CONFIGURATION
 func _validate_property(property: Dictionary) -> void:
 	var hide_list = []
@@ -34,36 +35,40 @@ func _validate_property(property: Dictionary) -> void:
 	if property.name in hide_list:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
 
+
 # MAIN FUNCTIONS
 func _ready() -> void:
-	_typing_time_gap = 1.0 / typing_speed
-	if !text.is_empty():
-		write(text)
+	if !Engine.is_editor_hint():
+		_typing_time_gap = 1.0 / typing_speed
+		if !text.is_empty():
+			write(text)
+
 
 func _process(delta: float) -> void:
-	if _typing && !_stopped && !_text_to_type.is_empty():
-		if _pause_timer <= 0:
-			# Compute how much chars shall be written on the current frame.
-			# More than 1 character can be written if the typing speed is higher than current framerate.
-			var next_chars := ""
-			while !_text_to_type.is_empty() && _typing_timer <= 0:
-				var next_char = _text_to_type[0]
-				_text_to_type = _text_to_type.erase(0)
-				next_chars += next_char
-				_typing_timer += _typing_time_gap
-				# However if a "pause" character is reached, do not type more characters for the current frame.
-				if pause_after_character && _is_pause_character(next_char):
-					_pause_timer = pause_duration
-					break
-			text += next_chars
-			# Play writing sound if exists and is not playing.
-			if writing_sound_player && !writing_sound_player.playing:
-				writing_sound_player.play()
-			_typing_timer -= delta
-		_pause_timer -= delta
-	else: # Finish typing
-		writing_done.emit()
-		_typing = false
+	if !Engine.is_editor_hint():
+		if _typing && !_stopped && !_text_to_type.is_empty():
+			if _pause_timer <= 0:
+				# Compute how much chars shall be written on the current frame.
+				# More than 1 character can be written if the typing speed is higher than current framerate.
+				var next_chars := ""
+				while !_text_to_type.is_empty() && _typing_timer <= 0:
+					var next_char = _text_to_type[0]
+					_text_to_type = _text_to_type.erase(0)
+					next_chars += next_char
+					_typing_timer += _typing_time_gap
+					# However if a "pause" character is reached, do not type more characters for the current frame.
+					if pause_after_character && _is_pause_character(next_char):
+						_pause_timer = pause_duration
+						break
+				text += next_chars
+				# Play writing sound if exists and is not playing.
+				if writing_sound_player && !writing_sound_player.playing:
+					writing_sound_player.play()
+				_typing_timer -= delta
+			_pause_timer -= delta
+		else: # Finish typing
+			writing_done.emit()
+			_typing = false
 
 
 func _is_pause_character(char: String) -> bool:
